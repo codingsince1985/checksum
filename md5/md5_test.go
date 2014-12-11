669d7365b665c15552c1365966ac078482beb5e3
Jerry Zhao <jchao@y7mail.com>
2014-12-11 21:08:40 +1100

    change package for test

--- md5/md5_test.go
+++ md5/md5_test.go
@@ -1,13 +1,14 @@
-package md5
+package md5_test
 
 import (
+	"github.com/codingsince1985/checksum/md5"
 	"testing"
 )
 
 func TestMd5sumFile(t *testing.T) {
 	file := "/home/jerry/Downloads/ubuntu-gnome-14.10-desktop-i386.iso"
 
-	md5sum, error := MD5sum(file)
+	md5sum, error := md5.MD5sum(file)
 	if error != nil || md5sum != "8041c76ef092626f4564baa0ffb595b6" {
 		t.Error("Md5sum(file) failed", md5sum, error)
 	}
@@ -16,7 +17,7 @@ func TestMd5sumFile(t *testing.T) {
 func TestMd5sumDir(t *testing.T) {
 	file := "/home/jerry/Downloads"
 
-	md5sum, error := MD5sum(file)
+	md5sum, error := md5.MD5sum(file)
 	if error != nil || md5sum != "" {
 		t.Error("Md5sum(dir) failed", md5sum, error)
 	}
