ca8816a67da9e801df8406c88fb700f89f2e18bc
Sascha <sascha.andres@outlook.com>
2019-04-12 08:18:01 +0200

    feat: implement Sum function

--- md5/md5.go
+++ md5/md5.go
@@ -42,6 +42,29 @@ here:
 }
 
 // Sum calculates the hash based on a provided hash provider
-func Sum(hash hash.Hash, filename string) (string, error) {
-	return "", nil
+func Sum(hashAlgorithm hash.Hash, filename string) (string, error) {
+	if info, err := os.Stat(filename); err != nil || info.IsDir() {
+		return "", err
+	}
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return "", err
+	}
+	defer func() { _ = file.Close() }()
+
+	buf := make([]byte, bufferSize)
+	reader := bufio.NewReader(file)
+here:
+	for {
+		switch n, err := reader.Read(buf); err {
+		case nil:
+			hashAlgorithm.Write(buf[:n])
+		case io.EOF:
+			break here
+		default:
+			return "", err
+		}
+	}
+	return fmt.Sprintf("%x", hashAlgorithm.Sum(nil)), nil
 }
